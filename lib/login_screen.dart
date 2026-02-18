e1ff1f8c3cfb115c91b6725a10dec18bf6cf1ddf
Hanu Shashwat <hanushashwat733@gmail.com>
2026-02-19 00:59:01 +0530

    Implemented BlocConsumer.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -20,7 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<AuthBloc, AuthState>(
+      body: BlocConsumer<AuthBloc, AuthState>(
         listener: (context, state) {
           if (state is AuthError) {
             ScaffoldMessenger.of(context).showSnackBar(
@@ -28,70 +28,80 @@ class _LoginScreenState extends State<LoginScreen> {
                 content: Text(state.error),
               ),
             );
-          } if (state is AuthSuccess) {
+          }
+          if (state is AuthSuccess) {
             Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(
-                builder: (context) => HomeScreen(),
-              ),
-              (route) => false
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+                    (route) => false
             );
           }
         },
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Image.asset('assets/images/signin_balls.png'),
-                const Text(
-                  'Sign in.',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 50,
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return Center(
+              child: CircularProgressIndicator(
+
+              ),
+            );
+          }
+          return SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Image.asset('assets/images/signin_balls.png'),
+                  const Text(
+                    'Sign in.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 50),
-                const SocialButton(
-                    iconPath: 'assets/svgs/g_logo.svg',
-                    label: 'Continue with Google'),
-                const SizedBox(height: 20),
-                const SocialButton(
-                  iconPath: 'assets/svgs/f_logo.svg',
-                  label: 'Continue with Facebook',
-                  horizontalPadding: 90,
-                ),
-                const SizedBox(height: 15),
-                const Text(
-                  'or',
-                  style: TextStyle(
-                    fontSize: 17,
+                  const SizedBox(height: 50),
+                  const SocialButton(
+                      iconPath: 'assets/svgs/g_logo.svg',
+                      label: 'Continue with Google'),
+                  const SizedBox(height: 20),
+                  const SocialButton(
+                    iconPath: 'assets/svgs/f_logo.svg',
+                    label: 'Continue with Facebook',
+                    horizontalPadding: 90,
                   ),
-                ),
-                const SizedBox(height: 15),
-                LoginField(
-                  hintText: 'Email',
-                  controller: emailController,
-                ),
-                const SizedBox(height: 15),
-                LoginField(
-                  hintText: 'Password',
-                  controller: passwordController,
-                ),
-                const SizedBox(height: 20),
-                GradientButton(
-                  onPressed: () {
-                    context.read<AuthBloc>().add(
-                      AuthLoginRequested(
-                          email: emailController.text.trim(),
-                          password: passwordController.text.trim()
-                      ),
-                    );
-                  },
-                ),
-              ],
+                  const SizedBox(height: 15),
+                  const Text(
+                    'or',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  LoginField(
+                    hintText: 'Email',
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  LoginField(
+                    hintText: 'Password',
+                    controller: passwordController,
+                  ),
+                  const SizedBox(height: 20),
+                  GradientButton(
+                    onPressed: () {
+                      context.read<AuthBloc>().add(
+                        AuthLoginRequested(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim()
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
